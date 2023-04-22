c3f6d16ed0d22078e25a964b04f442ceca38e642
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-04-22 13:27:30 -0300

    feat: implementing api consuming from our fruit type classifier model

---
+++ main.py
@@ -0,0 +1,48 @@
+import io
+import os
+
+import numpy as np
+import tensorflow as tf
+from fastapi import FastAPI, UploadFile
+
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+FRUIT_TYPE_CLASS_NAMES = ["APPLE", "BANANA", "ORANGE"]
+
+app = FastAPI()
+
+model = tf.keras.models.load_model(os.path.join("./models", "fruitclassifier.h5"))
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/predict/fruit-type")
+async def predict_fruit_type_from_image(file: UploadFile):
+    img = io.BytesIO(file.file.read())
+
+    fruit_type_class, confidence = __predict_image_from_path(img)
+
+    return {"predicted_class": fruit_type_class, "confidence": confidence}
+
+
+def __predict_image_from_path(img_path):
+    img = tf.keras.utils.load_img(img_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
+
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array, verbose=False)
+    score = tf.nn.softmax(predictions[0])
+
+    fruit_type_class = FRUIT_TYPE_CLASS_NAMES[np.argmax(score)]
+    confidence = 100 * np.max(score)
+    # print(
+    #     "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
+    #         fruit_type_class, confidence
+    #     )
+    # )
+
+    return fruit_type_class, confidence
