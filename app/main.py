245be3da6bf7171372ae85df479d8484859f4954
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-08-07 02:45:25 -0300

    feat: stage analysis to fruits

--- app/main.py
+++ app/main.py
@@ -5,17 +5,25 @@ import numpy as np
 import tensorflow as tf
 from fastapi import FastAPI, UploadFile
 
-IMG_HEIGHT = 180
-IMG_WIDTH = 180
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
 FRUIT_TYPE_CLASS_NAMES = ["APPLE", "BANANA", "ORANGE"]
+FRUIT_STAGES_CLASS_NAMES = ["OVERRIPE", "RAW", "RIPE"]
 
 app = FastAPI()
 
 # outside docker
-# model = tf.keras.models.load_model(os.path.join("../models", "fruitclassifier.h5"))
+identification_model = tf.keras.models.load_model(
+    os.path.join("../models", "fruit_type_classifier_v2.h5")
+)
+stages_model = tf.keras.models.load_model(
+    os.path.join("../models", "banana_stages_classifier_v3.h5")
+)
+
 
 # inside docker
-model = tf.keras.models.load_model(os.path.join("./fruitclassifier.h5"))
+# identification_model = tf.keras.models.load_model(os.path.join("./fruit_type_classifier_v2.h5"))
+# banana_stages_model = tf.keras.models.load_model(os.path.join("./banana_stages_classifier_v3.h5"))
 
 
 @app.get("/")
@@ -23,30 +31,48 @@ async def root():
     return "FruitLens API"
 
 
-@app.post("/predict/fruit-type")
-async def predict_fruit_type_from_image(file: UploadFile):
+@app.post("/predict/fruit")
+async def predict_fruit_from_image(file: UploadFile):
     img = io.BytesIO(file.file.read())
 
-    fruit_type_class, confidence = __predict_image_from_path(img)
+    img_array = __image_to_image_array(img)
 
-    return {"predicted_class": fruit_type_class, "confidence": confidence}
+    fruit_type_class, fruit_type_confidence = predict_identification(img_array)
+    fruit_stage_class, fruit_stage_confidence = None, None
+
+    if fruit_type_class == "BANANA":
+        fruit_stage_class, fruit_stage_confidence = predict_stages(img_array)
+
+    return {
+        "type": {"name": fruit_type_class, "confidence": fruit_type_confidence},
+        "stage": {"name": fruit_stage_class, "confidence": fruit_stage_confidence},
+    }
 
 
-def __predict_image_from_path(img_path):
+def __image_to_image_array(img_path):
     img = tf.keras.utils.load_img(img_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
 
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
 
-    predictions = model.predict(img_array, verbose=False)
+    return img_array
+
+
+def predict_identification(img_array):
+    predictions = identification_model.predict(img_array, verbose=False)
     score = tf.nn.softmax(predictions[0])
 
-    fruit_type_class = FRUIT_TYPE_CLASS_NAMES[np.argmax(score)]
+    _type = FRUIT_TYPE_CLASS_NAMES[np.argmax(score)]
     confidence = 100 * np.max(score)
-    # print(
-    #     "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
-    #         fruit_type_class, confidence
-    #     )
-    # )
 
-    return fruit_type_class, confidence
+    return _type, confidence
+
+
+def predict_stages(img_array):
+    predictions = stages_model.predict(img_array, verbose=False)
+    score = tf.nn.softmax(predictions[0])
+
+    stage = FRUIT_STAGES_CLASS_NAMES[np.argmax(score)]
+    confidence = 100 * np.max(score)
+
+    return stage, confidence
