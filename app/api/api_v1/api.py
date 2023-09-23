a3c7fc9272a40f5ebd832d8824b97c5b007b95b3
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-09-22 22:36:46 -0300

    feat: creating analyses route

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import fruit_types, fruit_maturation_stages
+from app.api.api_v1.endpoints import analyses, fruit_types, fruit_maturation_stages
 
 api_router = APIRouter()
+api_router.include_router(analyses.router, prefix="/analyses", tags=["analyses"])
 api_router.include_router(
-    fruit_types.router, prefix="/fruit-types", tags=["fruit_types"]
+    fruit_types.router, prefix="/fruit-types", tags=["fruit-types"]
 )
 api_router.include_router(
     fruit_maturation_stages.router,
---
+++ app/api/api_v1/endpoints/analyses.py
@@ -0,0 +1,14 @@
+from typing import Any, List
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+
+router = APIRouter()
+
+
+@router.get("/", response_model=str)
+def hello() -> Any:
+    return "Hello darkness, my old friend... I've come to talk with you again"
