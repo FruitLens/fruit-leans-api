7380376d33e248915c08adc762eaa56f7038b97f
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-09-26 21:49:21 -0300

    feat: creating endpoints to create and get analyses

--- app/api/api_v1/endpoints/analyses.py
+++ app/api/api_v1/endpoints/analyses.py
@@ -9,6 +9,30 @@ from app.api import deps
 router = APIRouter(prefix="/analyses", tags=["analyses"])
 
 
-@router.get("/", response_model=str)
+@router.get("/hello", response_model=str)
 def hello() -> Any:
     return "Hello darkness, my old friend... I've come to talk with you again"
+
+
+@router.get("/", response_model=List[schemas.Analysis])
+def get_all(db: Session = Depends(deps.get_db), skip: int = 0, limit: int = 100) -> Any:
+    return crud.analysis.get_all(db, skip=skip, limit=limit)
+
+
+@router.post("/", response_model=schemas.Analysis)
+def create_analysis(
+        *, db: Session = Depends(deps.get_db), analysis_in: schemas.AnalysisCreate
+) -> Any:
+    """
+    Create new analysis.
+    """
+    analysis = crud.analysis.get_by_telegram_img_id(
+        db, telegram_img_id=analysis_in.telegram_img_id
+    )
+    if analysis:
+        raise HTTPException(
+            status_code=400,
+            detail=f"The analysis of image {analysis_in.telegram_img_id} already exists.",
+        )
+
+    return crud.analysis.create(db, obj_in=analysis_in)
