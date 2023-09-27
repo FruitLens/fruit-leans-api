a966f88a8303c3cb09d6f790430b9233d811bfdd
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-09-26 21:43:34 -0300

    feat: adding relationship with analysis

--- app/models/fruit_maturation_stage.py
+++ app/models/fruit_maturation_stage.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
@@ -12,6 +13,17 @@ class FruitMaturationStage(Base):
     __tablename__ = "fruit_maturation_stage"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
+
+    model_predicted_analyses = relationship(
+        "Analysis",
+        foreign_keys="Analysis.model_predicted_fruit_maturation_stage_id",
+        back_populates="model_predicted_fruit_maturation_stage",
+    )
+    user_predicted_analyses = relationship(
+        "Analysis",
+        foreign_keys="Analysis.user_predicted_fruit_maturation_stage_id",
+        back_populates="user_predicted_fruit_maturation_stage",
+    )
 
     def __repr__(self):
         return f"FruitMaturationStage(id={self.id!r}, name={self.name!r})"
--- app/models/fruit_type.py
+++ app/models/fruit_type.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
@@ -12,6 +13,17 @@ class FruitType(Base):
     __tablename__ = "fruit_type"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
+
+    model_predicted_analyses = relationship(
+        "Analysis",
+        foreign_keys="Analysis.model_predicted_fruit_type_id",
+        back_populates="model_predicted_fruit_type",
+    )
+    user_predicted_analyses = relationship(
+        "Analysis",
+        foreign_keys="Analysis.user_predicted_fruit_type_id",
+        back_populates="user_predicted_fruit_type",
+    )
 
     def __repr__(self):
         return f"FruitType(id={self.id!r}, name={self.name!r})"
