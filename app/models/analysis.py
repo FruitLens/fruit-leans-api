250b769a95cc3a649a78eb56460ccf4a9842263b
c-santana-sp <eduardomarquesdesantana@gmail.com>
2023-09-26 21:36:42 -0300

    feat: creating analysis entity and migration

---
+++ app/models/analysis.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, Text, String
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+
+class Analysis(Base):
+    __tablename__ = "analysis"
+    id = Column(Integer, primary_key=True)
+    telegram_img_id = Column(String, unique=True, nullable=False)
+    telegram_conversation_id = Column(String, nullable=False)
+
+    model_predicted_fruit_type_id = Column(Integer, ForeignKey("fruit_type.id"))
+    user_predicted_fruit_type_id = Column(Integer, ForeignKey("fruit_type.id"))
+
+    model_predicted_fruit_type = relationship(
+        "FruitType",
+        foreign_keys=model_predicted_fruit_type_id,
+        back_populates="model_predicted_analyses",
+    )
+    user_predicted_fruit_type = relationship(
+        "FruitType",
+        foreign_keys=user_predicted_fruit_type_id,
+        back_populates="user_predicted_analyses",
+    )
+
+    model_predicted_fruit_maturation_stage_id = Column(
+        Integer, ForeignKey("fruit_maturation_stage.id")
+    )
+    user_predicted_fruit_maturation_stage_id = Column(
+        Integer, ForeignKey("fruit_maturation_stage.id")
+    )
+
+    model_predicted_fruit_maturation_stage = relationship(
+        "FruitMaturationStage",
+        foreign_keys=model_predicted_fruit_maturation_stage_id,
+        back_populates="model_predicted_analyses",
+    )
+    user_predicted_fruit_maturation_stage = relationship(
+        "FruitMaturationStage",
+        foreign_keys=user_predicted_fruit_maturation_stage_id,
+        back_populates="user_predicted_analyses",
+    )
+
+    user_approval = Column(Boolean)
+
+    def __repr__(self):
+        return f"Analysis(id={self.id!r})"
